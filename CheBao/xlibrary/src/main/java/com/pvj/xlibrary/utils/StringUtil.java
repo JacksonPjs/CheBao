a3a28548a64b24351ed62d7b4dd6484a3de6604a
JacksonPjs <shun837209130@163.com>
2018-07-16 14:47:35 +0800

    7.16

--- CheBao/xlibrary/src/main/java/com/pvj/xlibrary/utils/StringUtil.java
+++ CheBao/xlibrary/src/main/java/com/pvj/xlibrary/utils/StringUtil.java
@@ -192,8 +192,24 @@ public class StringUtil {
 
         Pattern p = null;  
         Matcher m = null;  
-        boolean b = false;   
-        p = Pattern.compile("^[1][2,3,4,5,6,7,8,9][0-9]{9}$"); // 验证手机号  
+        boolean b = false;
+        /**
+         * 大陆手机号码11位数，匹配格式：前三位固定格式+后8位任意数
+         * 此方法中前三位格式有：
+         * 13+任意数
+         * 14 +5至9
+         * 15+除4的任意数
+         * 18+除1和4的任意数
+         * 17+除9的任意数
+         * 147
+         */
+
+
+        String regExp = "^((13[0-9])|(14[5-9])|(15[^4])|(18[0,2,3,5-9])|(17[0-8])|(147))\\d{8}$";
+
+        p = Pattern.compile("^[1][3,4,5,6,7,8,9][0-9]{9}$"); // 验证手机号
+
+
         m = p.matcher(str);  
         b = m.matches();   
         return b;  
